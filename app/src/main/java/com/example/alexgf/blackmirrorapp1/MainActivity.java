a0de48b7c135abf16f4396c3f4e3ada581e1db93
ALEXGF <alexgf2703@gmail.com>
2019-01-06 00:11:43 -0500

    Versión 0.1 Se agregó ratingBar. Muestra en el mismo activity la calificación. Falta centrar los items.

--- app/src/main/java/com/example/alexgf/blackmirrorapp1/MainActivity.java
+++ app/src/main/java/com/example/alexgf/blackmirrorapp1/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.alexgf.blackmirrorapp1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RatingBar rbar1;
+
+    private TextView tv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rbar1 = (RatingBar)findViewById(R.id.rating_bar1);
+        tv1 = (TextView)findViewById(R.id.txt_companion_name);
+    }
+
+    public void calificar(View view){
+        Float rate = rbar1.getRating();
+        String qrate = rate.toString();
+
+        tv1.setText("You Rated: " + qrate);
     }
 }
